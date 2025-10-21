6ecc91f2762f20883d1b2d8f5d5add34227f30fe
Nanvule Faith <nanvule.faith.upti@gmail.com>
2025-10-21 09:02:49 +0300

    Dashboard modified

--- corruption/src/pages/Dashboard.jsx
+++ corruption/src/pages/Dashboard.jsx
@@ -1,35 +1,3 @@
-// import React from 'react';
-// import Side from '../components/Side';
-// import QuickStarts from '../components/QuickStats';
-// import RecentReports from '../components/RecentReports';
-// import Footer from '../components/Footer';
-// import '../styles/Dashboard.css';
-
-// function Dashboard() {
-//   return (
-//     <div className="dashboard">
-//       <Side />
-
-//       <div className="dashboard-main">
-//         <header className="dashboard-header">
-//           <h2>👋 Welcome back, User!</h2>
-//           <p>Here’s what’s happening with your reports today.</p>
-//         </header>
-
-//         <QuickStarts />
-
-//         <section className="dashboard-section">
-//           <RecentReports />
-//         </section>
-
-//         <Footer />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Dashboard;
-
 import React from 'react';
 import Side from '../components/Side';
 import QuickStarts from '../components/QuickStats';
