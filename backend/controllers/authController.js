8437bdce2c678cf427c75f2900da8fc0ce6189c8
wisdomjerry <wisdom.jeremiah.upti@gmail.com>
2025-11-26 16:46:38 +0300

    remove lax to none for cross-site cookies

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -41,7 +41,7 @@ const registerUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "Lax",
+      sameSite: "None",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
