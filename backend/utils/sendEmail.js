da835d21da0cadfa1b8435fac0f08dac2689afc1
wisdomjerry <wisdom.jeremiah.upti@gmail.com>
2025-11-27 13:03:48 +0300

    change to commonjs style

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,19 +2,13 @@ const sgMail = require("@sendgrid/mail");
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const sendEmail = async ({ to, subject, text, html }) => {
-  try {
-    await sgMail.send({
-      to,
-      from: process.env.SENDGRID_SENDER,
-      subject,
-      text,
-      html,
-    });
-  } catch (err) {
-    console.error("SendGrid email error:", err);
-    throw err;
-  }
+module.exports = async function sendEmail({ to, subject, text }) {
+  await sgMail.send({
+    to,
+    from: process.env.SENDGRID_SENDER,
+    subject,
+    text,
+  });
 };
 
-export default sendEmail;
+
