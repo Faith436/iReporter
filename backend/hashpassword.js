3c8c7bfef70ee3d03806ec519eb2a3bf03c15196
Nanvule Faith <nanvule.faith.upti@gmail.com>
2025-12-04 09:08:03 +0300

    updated the credentials in my env file

--- backend/hashpassword.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require("bcryptjs");
-
-const password = "wisdom1jeremiah123";
-
-bcrypt.hash(password, 10, (err, hash) => {
-  if (err) throw err;
-  console.log("Hashed password:", hash);
-});
